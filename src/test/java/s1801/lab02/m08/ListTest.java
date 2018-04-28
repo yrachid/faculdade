9fee456ac26175f61a954aa28dc402d0f52033fa
Yasser Rachid <yrachid@thoughtworks.com>
2018-04-28 10:34:58 -0300

    Testando listas encadeadas

---
+++ src/test/java/s1801/lab02/m08/ListTest.java
@@ -0,0 +1,113 @@
+package s1801.lab02.m08;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Enclosed.class)
+public class ListTest {
+
+    @Ignore
+    public static abstract class SetUp {
+
+        List list;
+
+        abstract void configure();
+
+        @Before
+        public void setUp() {
+            list = new List();
+            configure();
+        }
+    }
+
+    public static class WhenListIsEmpty extends SetUp {
+
+        @Override
+        void configure() { }
+
+        @Test
+        public void it_starts_empty() {
+            assertThat(list.isEmpty(), is(true));
+        }
+
+        @Test
+        public void has_null_first_node() {
+            assertThat(list.getFirst(), is(nullValue()));
+        }
+
+        @Test
+        public void has_null_last_node() {
+            assertThat(list.getLast(), is(nullValue()));
+        }
+
+    }
+
+    public static class WhenInsertingAtFront extends SetUp {
+
+        @Override
+        void configure() {
+            list.insertAtFront(1);
+        }
+
+        @Test
+        public void first_insertion_sets_both_first_and_last_as_same_node() {
+            assertThat(list.getFirst().getData(), is(1));
+            assertThat(list.getLast().getData(), is(1));
+        }
+
+        @Test
+        public void following_insertions_push_older_elements_to_the_tail_of_the_list() {
+            list.insertAtFront(2);
+
+            assertThat(list.getFirst().getData(), is(2));
+            assertThat(list.getLast().getData(), is(1));
+        }
+    }
+
+    public static class WhenInsertingAtBack extends SetUp {
+
+        @Override
+        void configure() {
+            list.insertAtBack(1);
+        }
+
+        @Test
+        public void first_insertion_sets_both_first_and_last_as_same_node() {
+            assertThat(list.getFirst().getData(), is(1));
+            assertThat(list.getLast().getData(), is(1));
+        }
+
+        @Test
+        public void following_insertions_push_older_elements_to_the_head_of_the_list() {
+            list.insertAtBack(2);
+
+            assertThat(list.getFirst().getData(), is(1));
+            assertThat(list.getLast().getData(), is(2));
+        }
+
+        @Test
+        public void following_insertion_sets_the_next_node_of_the_first_node() {
+            list.insertAtBack(2);
+
+            assertThat(list.getFirst().getNext().getData(), is(2));
+        }
+    }
+
+    @Ignore
+    public static class WhenInsertingAtFrontAndThenAtBack extends SetUp {
+
+        @Override
+        void configure() {
+            list.insertAtFront(1);
+            list.insertAtBack(2);
+
+        }
+    }
+}
